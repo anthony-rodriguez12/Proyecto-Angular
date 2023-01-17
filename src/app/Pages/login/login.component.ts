5fc1aa7ce6e1773b5adb8a1ea5c9ca983e2a5c11
Tony <rodriguezanthony981@gmail.com>
2023-01-17 13:47:26 -0500

    enviando cambios

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {NavigationExtras, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  ruta = '';
+  encrip: any;
+  user:any;
+  constructor(private router: Router) { }
+
+  alert: boolean = false;
+  
+  admins = [
+    { usuario:'Anthony',
+      password: '111',
+      img: 'https://imgs.search.brave.com/9I3Yub7c_zoccayRACNlCGAMzNtsyubakFjpvsunEhY/rs:fit:500:621:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2VkL2Zh/LzU5L2VkZmE1OTdm/N2Q2NGRjNTY3ODI2/NjMxYTBiOTE2MWY0/LmpwZw'  
+    },
+    { usuario:'Nallely',
+      password: '222',
+      img: 'https://imgs.search.brave.com/DV22PKGSHEy-uWZxs0hWQgSyxlxhIzKFRt4K5p5tn5Q/rs:fit:474:474:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vNDc0/eC9jMi82MS8xOC9j/MjYxMTgwMGYxMTQw/NzQ3YmFmZjExNDdl/MzM2Zjc0Mi5qcGc'
+      },
+    { usuario:'Ati', 
+      password: '333',
+      img: 'https://imgs.search.brave.com/UpKbTZ5p-sBI-a6bAABBGWon7eUW0V-yDOTpIxOaIkk/rs:fit:650:516:1/g:ce/aHR0cDovLzMuYnAu/YmxvZ3Nwb3QuY29t/Ly1Ba3RzT1dZWENB/MC9VQ1ROZDA5bjRD/SS9BQUFBQUFBQU1f/SS9oTlVvblVpY1hs/US9zMTYwMC9jaGlj/bytub3JtYWwuanBn'
+      },
+    { usuario:'Madelyne',
+      password: '444',
+      img: 'https://imgs.search.brave.com/BboZuhela0RefiP5W-neB_W3oXe7FHkP5NM7xJO4aCE/rs:fit:736:736:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vNzM2/eC84Yi8zZi8xMy84/YjNmMTNhNGQ4Njc3/ODc3ZWYyZGMyNDdk/MmEyNjE3Yy5qcGc'
+      },
+  ]
+
+  usuarioLogin = new FormGroup({
+    usuario: new FormControl('',Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+
+
+  Users = {
+    contador: 0
+  };
+  valid:any = ''
+  onSubmit(){
+    const { usuario, password } = this.usuarioLogin.value;
+    console.log(usuario);
+    this.valid = 'was-validated'
+    this.admins.some((user) => {
+      if(user.usuario === usuario && user.password === password){
+        this.router.navigate(['/productos']);
+        this.myRuta()
+        
+        this.Users.contador = 0
+        //this.encrip = JSON.stringify(user);
+        //this.cookies.set("Data",this.encrip);
+      };
+      this.Users.contador++; 
+    });
+    if(this.Users.contador === 4 ) {
+      this.alert = true;
+      setTimeout(() => this.alert=false, 4000);
+    }
+    
+  }
+  cancelar(){
+    this.router.navigate(['/']);
+  }
+
+  myRuta(){
+    this.ruta = this.router.url;
+    console.log(this.ruta)
+    return this.ruta
+  }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './Pages/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HomeComponent } from './Pages/home/home.component';
 import { NosotrosComponent } from './Pages/nosotros/nosotros.component';
 import { LocalesComponent } from './Pages/locales/locales.component';
 import { ProductosComponent } from './Pages/productos/productos.component';
 import { ContactarComponent } from './Pages/contactar/contactar.component';
 import { LoginComponent } from './Pages/login/login.component';
+
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input'
+import {MatCardModule} from '@angular/material/card'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
@@ -28,8 +39,11 @@ import { LoginComponent } from './Pages/login/login.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
-    NgbCarouselModule
+    NgbCarouselModule,
+    MatButtonModule, ReactiveFormsModule, MatInputModule, 
+    MatCardModule, MatToolbarModule, MatIconModule, MatDialogModule, MatTableModule
   ],
+  entryComponents: [LoginComponent],
   providers: [NgbCarouselConfig],
   bootstrap: [AppComponent]
 })
